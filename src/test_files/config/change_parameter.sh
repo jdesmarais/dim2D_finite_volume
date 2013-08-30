4fc875a8d3c98dbceb1652c0f322d03f15462943
Julien Desmarais <desmaraisjulien@gmail.com>
2013-08-30 09:59:43 +0200

    Implementing configuration script to modify the source code according to the input file requirements

---
+++ src/test_files/config/change_parameter.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+PATH_OUTPUT='inputs.txt' #default path for the output
+PATH_INPUT='inputs.txt'  #default path for the input
+PARAM_NAME=''            #default name of the parameter changed
+PARAM_VALUE=''           #default value of the parameter changed
+
+
+#manage the options
+#--------------------------------------------
+while getopts i:o:p:v:h option
+  do
+  case $option in
+
+      #get the path of the input file
+      #submitted by the user
+      i)
+	  if test -e $OPTARG
+	  then
+	      PATH_INPUT=$OPTARG
+	  fi
+	  ;;
+      
+      #get the path of the output file
+      #submitted by the user
+      o)
+	  PATH_OUTPUT=$OPTARG
+	  ;;
+
+      #get the name of the parameter changed
+      #by the user
+      p)
+	  PARAM_NAME=$OPTARG
+	  ;;
+
+      #get the value of the parameter changed
+      #by the user
+      v)
+	  PARAM_VALUE=$OPTARG
+	  ;;
+
+      #display help
+      h)
+          echo "invalid option"
+	  
+	  echo "-i: input file path"
+	  echo "-o: output file path"
+	  echo "-p: name of the parameter changed"
+	  echo "-v: value of the parameter changed"
+	  ;;
+
+      #default
+      \?)
+          echo "invalid option"
+	  
+	  echo "-i: input file path"
+	  echo "-o: output file path"
+	  echo "-p: name of the parameter changed"
+	  echo "-v: value of the parameter changed"
+      
+  esac
+done
+
+
+#change the parameter by the value given by the user in the input file
+#---------------------------------------------------------------------
+if [ "$PATH_INPUT" == "$PATH_OUTPUT" ]
+then
+    TEMP_FILE=true
+    PATH_OUTPUT='temp.txt'
+else
+    TEMP_FILE=false
+fi
+
+sed -e s/$PARAM_NAME[' ']*'='[' ']*[-0-9a-zA-Z_\\.e-]*/$PARAM_NAME' = '$PARAM_VALUE/g \
+    < $PATH_INPUT > $PATH_OUTPUT
+
+if $TEMP_FILE
+then
+    mv 'temp.txt' $PATH_INPUT
+fi
---
+++ src/test_files/config/get_parameter.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+PATH_INPUT='/home/jdesmarais/Code/nemeanlion/input/inputs_dim1d_interface_pml_cst.txt'
+
+
+#manage the options
+#--------------------------------------------
+while getopts i:p: option
+  do
+  case $option in
+
+      #get the path of the input file
+      #submitted by the user
+      i)
+	  if test -e $OPTARG
+	      then
+	      PATH_INPUT=$OPTARG
+	  else
+	      echo "input file $OPTARG does not exist"
+	      echo "default input file used"
+	  fi
+	  ;;
+
+      #get the name of the parameter
+      #extracted from the input file
+      p)
+	  PARAM_NAME=$OPTARG
+	  ;;
+
+      #default
+      \?)
+          echo "invalid option"
+	  echo "-i: input file"
+      
+  esac
+done
+
+
+
+#extract the line where the parameter is mentionned
+#--------------------------------------------------
+line=$(grep $PARAM_NAME[' ']*'='[' ']*[0-9\\.]* $PATH_INPUT)
+
+
+#extract the value out of the line
+#---------------------------------
+#value=$(echo $line | sed -e  s/[A-Za-z0-9_" "]*"="[" "]*/''/g)
+value=$(echo $line | sed -e  s/[^"=""!"]*"="[" "]*/''/g)
+value=$(echo $value | sed -e  s/[' ']*'!'[^'!']*/''/g)
+echo $value
