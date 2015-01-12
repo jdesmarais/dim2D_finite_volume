4d4590d9f90fbed4784b6414a79d28863f885d03
Julien Desmarais <desmaraisjulien@gmail.com>
2015-01-12 20:11:03 +0100

    Implementing the creation of inputs for small and large domains: tested

---
+++ scripts_py/sm_lg_domain_automatization/create_sm_lg_inputs.py
@@ -0,0 +1,416 @@
+#!/usr/bin/python
+
+'''
+@description
+create two input files to run the DIM2D simulation on two domains:
+inputs_sm_domain.txt:  inputs for the small domain simulation
+inputs_lg_domain.txt:  inputs for the large domain simulation
+'''
+
+import os
+import sys
+import getopt
+import subprocess
+import shlex
+import shutil
+
+from library_sm_lg_inputs import (get_we,
+                                  get_interface_length,
+                                  get_bubble_diameter,
+                                  get_domain_length,
+                                  get_interface_space_step,
+                                  get_small_domain_extent,
+                                  get_cv_r,
+                                  get_max_speed_of_sound,
+                                  get_dt_max,
+                                  get_simulation_time,
+                                  get_detail_print,
+                                  get_large_domain_extent)
+                                       
+
+# display the help for the program
+def display_help():
+    '''
+    @description:
+    display program help
+    '''
+    print ''
+    print 'configure two inputs files to run DIM2D on a small and'
+    print 'a large domain'
+    print ''
+    print 'options:'
+    print '--------'
+    print '-h (--help)          : display this help'
+    print '-T (--temperature=)  : temperature'
+    print '-v (--flow_velocity=): flow_velocity'
+    print '-i (--model_input=)  : input file used as template'
+    print '-s (--sm_domain=)    : filename for the small domain input'
+    print '-l (--lg_domain=)    : filename for the large domain input'
+    print ''
+    print 'example:'
+    print '---------'
+    print 'configure input files at T=0.95 and flow_velocity=0.1 using'
+    print 'template.txt as a template for the input file. The default'
+    print 'outputs are inputs_sm_domain.txt and inputs_lg_domain.txt'
+    print './config -i template.txt - T 0.95 -v 0.1'
+    print ''
+
+
+#parse the input options
+def parse_argv(argv):
+    '''
+    @description:
+    parse the program options
+    '''
+
+    #default values
+    smDomainInputDefault = 'inputs_sm_domain.txt'
+    lgDomainInputDefault = 'inputs_lg_domain.txt'
+
+
+    # store the options and the arguments
+    # in opts, args
+    try:
+        opts, args = getopt.getopt(argv,
+                                   "hT:v:i:s:l:",
+                                   ["help",
+                                    "temperature",
+                                    "flow_velocity",
+                                    "model_input",
+                                    "sm_domain",
+                                    "lg_domain"])
+    except getopt.GetoptError:
+        display_help()
+        sys.exit(2)
+
+    if(len(opts)==0):
+        display_help()
+        sys.exit(2)
+
+
+    # initialization of the boolean controlling if
+    # all the needed inputs are provided
+    temperatureProvided  = False
+    flowVelocityProvided = False
+    modelInputProvided   = False
+
+    smDomainInput = smDomainInputDefault
+    lgDomainInput = lgDomainInputDefault
+
+
+    # analyse the program options
+    for opt, arg in opts:
+
+        if opt in ("-h", "--help"):
+            display_help()
+            sys.exit(2)
+
+        elif opt in ("-T", "--temperature"):
+            temperature = float(arg)
+
+            if(temperature>0 and temperature<1):
+                temperatureProvided=True
+            else:
+                print 'the temperature should be in ]0,1['
+            
+        elif opt in ("-v","--flow_velocity"):
+            flow_velocity = float(arg)
+
+            if(flow_velocity>0 and flow_velocity<1):
+                flowVelocityProvided=True
+            else:
+                print 'the flow velocity should be in ]0,1['
+
+        elif opt in ("-i","--model_input"):
+            modelInputPath = arg
+
+            if(os.path.isfile(modelInputPath)):
+                modelInputProvided=True
+            else:
+                print modelInputPath+' does not exist'
+
+        elif opt in ("-s", "--sm_domain"):
+            smDomainInput = arg
+
+        elif opt in ("-l", "--lg_domain"):
+            lgDomainInput = arg
+
+    inputsProvided = temperatureProvided
+    inputsProvided = inputsProvided and flowVelocityProvided
+    inputsProvided = inputsProvided and modelInputProvided
+
+    if(not inputsProvided):
+        display_help()
+        sys.exit('***some inputs were not provided***')
+
+    else:
+        inputs = {'temperature'  : temperature,
+                  'flow_velocity': flow_velocity,
+                  'model_input'  : modelInputPath,
+                  'sm_domain'    : smDomainInput,
+                  'lg_domain'    : lgDomainInput}
+                    
+        return inputs
+
+
+# extract 'value' for parameter 'param' from the text
+# file 'filePath' if the definition of 'value' is as
+# follow 'param = value'
+def get_parameter(param,filePath):
+    '''
+    @description:
+    extract 'value' for parameter 'param' from the text
+    file 'filePath' if the definition of 'value' is as
+    follow 'param = value'
+    '''
+
+    # check whether the file exists
+    if(os.path.isfile(filePath)):
+
+        # path for 'get_parameter.sh' script
+        getParameterScriptPath = os.getenv('augeanstables')+"/src/test_files/config/get_parameter.sh"
+        
+        # check whether the 'get_parameter.sh'
+        # script exists
+        if(os.path.isfile(getParameterScriptPath)):
+               
+           # create process extracting the parameter
+           # using the 'get_parameter.sh' script
+           cmd=getParameterScriptPath+' -p '+param+' -i '+filePath
+           args = shlex.split(cmd)
+           output = subprocess.Popen(args,stdout=subprocess.PIPE).communicate()[0]
+
+        else:
+
+            print '***get_parameter.sh script does not exist***'
+            output = ''
+        
+    else:
+
+        print '***'+filePath+' does not exist***'
+        output = ''
+
+    return output.strip()
+    
+
+# compute the inputs that are modified in the template.txt
+# template input file to run the DIM2D simulation on a small
+# and on a large domains
+def get_inputsToBeModified(temperature,
+                           flow_velocity,
+                           nb_pts_in_interface,
+                           ratio_bubble_interface,
+                           CFL_constant,
+                           ratio_interface_influence,
+                           total_nb_files):
+    '''
+    @description:
+    compute the inputs that are modified in the template.txt
+    template input file to run the DIM2D simulation on a small
+    and on a large domains
+    '''
+
+    # extract length_c, dim2d_a, dim2d_b, dim2d_M, dim2d_cv, dim2d_R
+    # and dim2d_K from the dim2d_parameters.f fortran file
+    dim2dParamPath = os.getenv('augeanstables')
+    dim2dParamPath+='/src/physical_models/dim2d/dim2d_parameters.f'
+
+    if(os.path.isfile(dim2dParamPath)):
+        length_c  = float(get_parameter('length_c', dim2dParamPath))
+        dim2d_a   = float(get_parameter( 'dim2d_a', dim2dParamPath))
+        dim2d_b   = float(get_parameter( 'dim2d_b', dim2dParamPath))
+        dim2d_M   = float(get_parameter( 'dim2d_M', dim2dParamPath))
+        dim2d_cv  = float(get_parameter('dim2d_cv', dim2dParamPath))
+        dim2d_R   = float(get_parameter( 'dim2d_R', dim2dParamPath))
+        dim2d_K   = float(get_parameter( 'dim2d_K', dim2dParamPath))
+
+    else:
+        sys.exit('*** '+dim2dParamPath+' does not exist***')
+
+
+    # compute the Weber number
+    we = get_we(length_c, dim2d_a, dim2d_b, dim2d_M, dim2d_K)
+
+
+    # compute the interface length from the temperature
+    interface_lgh = get_interface_length(we,temperature)
+
+
+    # compute the bubble diameter from the interface length
+    bubble_diameter = get_bubble_diameter(interface_lgh,
+                                          ratio_bubble_interface)
+
+    # compute the domain length from the bubble diameter
+    domain_length = get_domain_length(bubble_diameter,
+                                      interface_lgh,
+                                      ratio_interface_influence)
+
+
+    # compute the maximum space step from the interface length
+    dx_max = get_interface_space_step(interface_lgh,
+                                      nb_pts_in_interface)
+
+
+    # compute the extent of the small domain as a matrix:
+    # x_min : small_domain_extent[0][0]
+    # x_max : small_domain_extent[1][0]
+    # y_min : small_domain_extent[0][1]
+    # y_max : small_domain_extent[1][1]
+    small_domain_extent = get_small_domain_extent(domain_length,
+                                                  dx_max)
+
+
+    # compute the reduced heat capacity
+    cv_r = get_cv_r(dim2d_M,dim2d_cv,dim2d_R)
+
+
+    # compute the maximum speed of sound in the flow
+    speed_of_sound = get_max_speed_of_sound(temperature,cv_r)
+
+
+    # compute the maximum time step ensuring numerical stability
+    speed_max = speed_of_sound + abs(flow_velocity)
+    dt_max    = get_dt_max(dx_max,speed_max,CFL_constant)
+
+
+    # determine the simulation time needed to let the bubble
+    # leave the computational domain
+    simulation_time = get_simulation_time(domain_length,
+                                          bubble_diameter,
+                                          interface_lgh,
+                                          ratio_interface_influence,
+                                          flow_velocity)
+
+
+    # determine the detail print
+    detail_print = get_detail_print(total_nb_files,
+                                    simulation_time,
+                                    dt_max)
+
+
+    # determine the large domain extent
+    large_domain_extent = get_large_domain_extent(small_domain_extent,
+                                                  dx_max,dx_max,
+                                                  flow_velocity,
+                                                  speed_of_sound,
+                                                  simulation_time)
+
+
+    # gather the inputs to be modified in dictionnaries
+    inputsToBeModified_sm_domain = {
+        'detail_print'    : detail_print,
+        'dt'              : dt_max,
+        't_max'           : simulation_time,
+        'dx'              : dx_max,
+        'x_min'           : small_domain_extent[0][0],
+        'x_max'           : small_domain_extent[1][0],
+        'dy'              : dx_max,
+        'y_min'           : small_domain_extent[0][1],
+        'y_max'           : small_domain_extent[1][1],
+        'flow_velocity'   : flow_velocity,
+        'temperature'     : temperature}
+
+    inputsToBeModified_lg_domain = {
+        'detail_print'    : detail_print,
+        'dt'              : dt_max,
+        't_max'           : simulation_time,
+        'dx'              : dx_max,
+        'x_min'           : large_domain_extent[0][0],
+        'x_max'           : large_domain_extent[1][0],
+        'dy'              : dx_max,
+        'y_min'           : large_domain_extent[0][1],
+        'y_max'           : large_domain_extent[1][1],
+        'flow_velocity'   : flow_velocity,
+        'temperature'     : temperature}
+
+        
+    return [inputsToBeModified_sm_domain,
+            inputsToBeModified_lg_domain]
+
+
+# modify the parameters in the template.txt file to create 
+# a new input file
+def create_inputFile(paramModified,
+                     templateFilePath,
+                     newInputFilePath):
+    '''
+    @description:
+    modify the parameters in the template.txt file to
+    create a new input file
+    '''    
+    
+
+    #1) verify that the templateFilePath exists
+    if(os.path.isfile(templateFilePath)):
+
+
+        #2) make a copy of the template file for the output
+        #   file newInputFilePath
+        if(os.path.isfile(newInputFilePath)):
+            os.remove(newInputFilePath)
+        
+        shutil.copyfile(templateFilePath,newInputFilePath)
+
+        
+        #3) read the parameters to be modified in paramModified
+        #   and replace them in newInputFilePath
+        for key, value  in paramModified.items():
+
+            cmd=os.getenv('augeanstables')+"/src/test_files/config/change_parameter.sh"
+            cmd+=" -i "+str(newInputFilePath)
+            cmd+=" -o "+str(newInputFilePath)
+            cmd+=" -p "+key
+            cmd+=" -v "+str(value)
+            subprocess.call(cmd, shell=True)
+
+    else:
+        sys.exit('***the file '+templateFilePath+' does not exist***')
+        
+
+    return
+
+
+if __name__=="__main__":
+
+
+    # define the constants used when computing
+    # the inputs on the small and large scale
+    # domains
+    nb_pts_in_interface = 10
+    ratio_bubble_interface = 2
+    CFL_constant = 0.2
+    ratio_interface_influence = 2.0
+    total_nb_files = 1000
+
+
+    # extract the command line inputs
+    inputs = parse_argv(sys.argv[1:])
+
+
+    # determine the inputs to be modified in
+    # template.txt to run the simulation on
+    # small and large scale domains
+    [inputs_sm_domain, inputs_lg_domain] = get_inputsToBeModified(
+        inputs['temperature'],
+        inputs['flow_velocity'],
+        nb_pts_in_interface,
+        ratio_bubble_interface,
+        CFL_constant,
+        ratio_interface_influence,
+        total_nb_files)
+    
+    
+    # create the input file for the small
+    # domain simulation
+    create_inputFile(inputs_sm_domain,
+                     inputs['model_input'],
+                     inputs['sm_domain'])
+
+
+    # create the input file for the large
+    # domain simulation
+    create_inputFile(inputs_lg_domain,
+                     inputs['model_input'],
+                     inputs['lg_domain'])
+
+    
--- scripts_py/sm_lg_domain_automatization/library_sm_lg_inputs.py
+++ scripts_py/sm_lg_domain_automatization/library_sm_lg_inputs.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+'''
+@description
+useful functions to compute the intermediate parameters when determining
+the input parameters for the small and large domains simulations of DIM2D
+to estimate the efficiency of the buffer layer technique
+'''
+
 import sys         #for system fcts
 #import getopt
 #import subprocess
@@ -34,9 +41,41 @@ def get_mass_density_liquid(T):
     return 1.0+2.08*math.sqrt(1.0-T)
 
 
+# get the reduced constant volume heat capacity
+def get_cv_r(dim1d_M,dim1d_cv,dim1d_R):
+    '''
+    @description:
+    compute the reduced constant volume heat
+    capacity
+    '''
+    
+    return dim1d_M*dim1d_cv/dim1d_R
+
+
+# compute the reduced speed of sound from the
+# mass density, the temperature and the specific
+# heat capacity
+def get_speed_of_sound(mass,T,cv_r):
+    '''
+    @description:
+    compute the reduced speed of sound from the
+    mass density, the temperature and the specific
+    heat capacity
+    '''
+
+    pressure  = 8.0*mass*T/(3.0-mass) - 3.0*mass**2
+    b         = math.sqrt((pressure+3.0*mass**2)/
+                          (cv_r*(pressure+mass**2*(-3.0+2.0*mass))))
+    a         = 1.0/math.sqrt(1.0+b**2)
+    d         = math.sqrt(mass**3)*math.sqrt((3.0-mass)/
+                                             (pressure+mass**2*(-3.0+2.0*mass)))
+
+    return math.sqrt(3)*mass/(a*d)
+
+
 # compute the maximum reduced speed of sound in a multi-phase fluid
 # of saturated liquid and vapor at equilibrium
-def get_max_speed_of_sound(gamma,T):
+def get_max_speed_of_sound(T,cv_r):
     '''
     @description:
     compute the maximum reduced speed of sound in a
@@ -46,12 +85,10 @@ def get_max_speed_of_sound(gamma,T):
     '''
 
     mass      = get_mass_density_vapor(T)
-    pressure  = 8.0*mass*T/(3.0-mass) - 3.0*mass**2
-    speed_vap = math.sqrt(gamma*pressure/mass)
+    speed_vap = get_speed_of_sound(mass,T,cv_r)
 
     mass      = get_mass_density_liquid(T)
-    pressure  = 8.0*mass*T/(3.0-mass) - 3.0*mass**2
-    speed_liq = math.sqrt(gamma*pressure/mass)
+    speed_liq = get_speed_of_sound(mass,T,cv_r)
 
     return max(speed_vap,speed_liq)
 
@@ -122,18 +159,20 @@ def get_bubble_diameter(interface_lgh,ratio_bubble_interface):
     return ratio_bubble_interface*interface_lgh
 
 
-# compute the computational domain extent from the bubble
-# diameter and the ratio between the computational domain
-# and the bubble
-def get_domain_length(bubble_diameter,ratio_domain_bubble):
+# compute the length of the computational domain from
+# the bubble diameter and distance the bubble should be
+# from the edge of the doamin to neglect the interactions
+def get_domain_length(bubble_diameter,
+                      interface_length,
+                      ratio_interface_influence):
     '''
     @description
     compute the length of the computational domain from
-    the bubble diameter and the ratio between the
-    computational domain and the bubble
+    the bubble diameter and distance the bubble should be
+    from the edge of the doamin to neglect the interactions
     '''
     
-    return ratio_domain_bubble*bubble_diameter
+    return bubble_diameter+2.0*interface_length*ratio_interface_influence
 
 
 #determine the extent of the small domain simulation
@@ -143,7 +182,7 @@ def get_small_domain_extent(domain_length,dx):
     determine the extent of the small domain simulation
     '''
 
-    nb_grdpts_half_domain = math.ceil(domain_lgh/(2.0*dx))
+    nb_grdpts_half_domain = math.ceil(domain_length/(2.0*dx))
     
     small_domain_extent = [[0 for x in range(2)] for x in range(2)]
     small_domain_extent[0][0] = 0.0 - nb_grdpts_half_domain*dx
@@ -197,7 +236,9 @@ def get_simulation_time(domain_lgh,
     #simulation is
     total_distance = domain_lgh/2.0+ratio_interface_influence*interface_lgh+bubble_diameter/2.0
 
-    return total_distance/flow_velocity
+    return float(math.ceil(
+            total_distance/flow_velocity*
+            10**4))/(10**4)
 
 
 # get the detail_print such that the total number of files
@@ -209,14 +250,17 @@ def get_detail_print(total_nb_files, simulation_time, dt):
     written fits what is asked by the user
     '''
 
-    return dt*total_nb_files/simulation_time
+    return float(math.ceil(
+            dt*total_nb_files/simulation_time
+            *10**4))/(10**4)
 
 
 #get the extent of the large computational domain
 def get_large_domain_extent(small_domain_extent,
                             dx,dy,
                             flow_velocity,
-                            speed_of_sound):
+                            speed_of_sound,
+                            simulation_time):
     '''
     @description
     get the extent of the large domain such that the perturbations
@@ -247,15 +291,15 @@ def get_large_domain_extent(small_domain_extent,
 
 def print_comparison(name_var,var,var_test):
 
-    print '%20s'%name_var, '%6.3f'%var,'%6.3f'%var_test
+    print '%20s'%name_var, '%7.4f'%var,'%7.4f'%var_test
 
 
 def print_comparison_matrix(name_var,var,var_test):
 
-    print '%20s'%name_var, '%6.3f'%var[0][0],'%6.3f'%var_test[0][0]
-    print '%20s'%' '     , '%6.3f'%var[1][0],'%6.3f'%var_test[1][0]
-    print '%20s'%' '     , '%6.3f'%var[0][1],'%6.3f'%var_test[0][1]
-    print '%20s'%' '     , '%6.3f'%var[1][1],'%6.3f'%var_test[1][1]
+    print '%20s'%name_var, '%7.4f'%var[0][0],'%7.4f'%var_test[0][0]
+    print '%20s'%' '     , '%7.4f'%var[1][0],'%7.4f'%var_test[1][0]
+    print '%20s'%' '     , '%7.4f'%var[0][1],'%7.4f'%var_test[0][1]
+    print '%20s'%' '     , '%7.4f'%var[1][1],'%7.4f'%var_test[1][1]
     
 
 if __name__ == "__main__":
@@ -264,40 +308,41 @@ if __name__ == "__main__":
     T_test                         = 0.99
     md_vap_test                    = 0.814
     md_liq_test                    = 1.208
-    gamma_test                     = 0.5
-    speed_of_sound_test            = 0.768
+    cv_r_test                      = 0.0509
+    speed_of_sound_test            = 12.0655
     length_c_test                  = 0.5
     dim1d_a_test                   = 0.1
     dim1d_b_test                   = 0.2
     dim1d_M_test                   = 0.3
+    dim1d_cv_test                  = 1410.
+    dim1d_R_test                   = 8314.0
     dim1d_K_test                   = 0.4
-    we_test                        = 0.231
-    interface_lgh_test             = 67.799
+    we_test                        = 0.2315
+    interface_lgh_test             = 67.7994
     nb_pts_test                    = 10
-    dx_max_test                    = 6.780
+    dx_max_test                    = 6.7799
     ratio_bubble_interface_test    = 2
-    diameter_test                  = 135.599
-    ratio_domain_bubble_test       = 1.5
-    domain_lgh_test                = 203.398
+    diameter_test                  = 135.5988
+    domain_lgh_test                = 338.9970
     CFL_constant_test              = 0.1
-    dt_max_test                    = 0.882
+    dt_max_test                    = 0.0561
     ratio_interface_influence_test = 1.5
     flow_velocity_test             = 0.1
-    simulation_time_test           = 2711.976
+    simulation_time_test           = 3389.9699
     total_nb_files_test            = 10
-    detail_print_test              = 0.003
+    detail_print_test              = 0.0002
 
     small_domain_extent_test = [[0 for x in range(2)] for x in range(2)]
-    small_domain_extent_test[0][0] = -108.4784
-    small_domain_extent_test[1][0] =  108.4784
-    small_domain_extent_test[0][1] = -108.4784
-    small_domain_extent_test[1][1] =  108.4784
+    small_domain_extent_test[0][0] = -176.2774
+    small_domain_extent_test[1][0] =  176.2774
+    small_domain_extent_test[0][1] = -176.2774
+    small_domain_extent_test[1][1] =  176.2774
 
     large_domain_extent_test = [[0 for x in range(2)] for x in range(2)] 
-    large_domain_extent_test[0][0] = - 1288.181
-    large_domain_extent_test[1][0] =   1288.181
-    large_domain_extent_test[0][1] = - 1288.181
-    large_domain_extent_test[1][1] =   1288.181
+    large_domain_extent_test[0][0] = - 20800.7332
+    large_domain_extent_test[1][0] =   20800.7332
+    large_domain_extent_test[0][1] = - 20800.7332
+    large_domain_extent_test[1][1] =   20800.7332
     
 
     #test: get_mass_density_vapor
@@ -306,8 +351,11 @@ if __name__ == "__main__":
     #test: get_mass_density_liquid
     md_liq = get_mass_density_liquid(T_test)
     
+    #test: get_cv_r
+    cv_r   = get_cv_r(dim1d_M_test, dim1d_cv_test, dim1d_R_test)
+
     #test: get_max_speed_of_sound
-    speed_of_sound  = get_max_speed_of_sound(gamma_test,T_test)
+    speed_of_sound  = get_max_speed_of_sound(T_test,cv_r)
 
     #test: get_we
     we = get_we(length_c_test, dim1d_a_test, dim1d_b_test, dim1d_M_test, dim1d_K_test)
@@ -322,7 +370,7 @@ if __name__ == "__main__":
     diameter = get_bubble_diameter(interface_lgh,ratio_bubble_interface_test)
 
     #test: get_domain_length
-    domain_lgh = get_domain_length(diameter,ratio_domain_bubble_test)
+    domain_lgh = get_domain_length(diameter,interface_lgh,ratio_interface_influence_test)
 
     #test: get_dt_max
     dt_max = get_dt_max(dx_max,speed_of_sound,CFL_constant_test)
@@ -345,11 +393,13 @@ if __name__ == "__main__":
     large_domain_extent = get_large_domain_extent(small_domain_extent_test,
                                                   dx_max,dx_max,
                                                   flow_velocity_test,
-                                                  speed_of_sound)
+                                                  speed_of_sound,
+                                                  simulation_time)
 
     #compare data
     print_comparison('md_vap:',md_vap, md_vap_test)
     print_comparison('md_liq:',md_liq, md_liq_test)
+    print_comparison('cv_r:',cv_r, cv_r_test)
     print_comparison('speed_of_sound:',speed_of_sound, speed_of_sound_test)
     print_comparison('we:',we, we_test)
     print_comparison('interface_lgh:',interface_lgh, interface_lgh_test)
